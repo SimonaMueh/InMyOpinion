4a16c0bf50b99ed1793502c9172febe4007b6a26
Monalisarah <alles0815@gmx.ch>
2017-07-10 11:46:45 +0200

    refactoring Topics ResultPage

---
+++ src/containers/TopicResult/index.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class TopicResult extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:8080/topics')
+    .then(parseJSON => parseJSON.json())
+    .then(data => {
+      this.props.dispatch({type: 'GETTOPICS', topics: data})
+    });
+  }
+
+  render() {
+    const topic = this.props.topics[this.props.paramsId - 1];
+    const total = topic.votes.length;
+
+    let voteTrue = 0;
+    const countTrue = topic.votes.filter((vote) => {
+      if (vote.selection === true) {
+        voteTrue++;
+      }
+      return voteTrue;
+    });
+
+    let voteFalse = 0;
+    const countFalse = topic.votes.filter((vote) => {
+      if (vote.selection === false) {
+        voteFalse++;
+      }
+      return voteFalse;
+    });
+    return (
+      <div>
+        <ul>
+          total: {total}<br></br>
+          yes: {voteTrue}<br></br>
+          no: {voteFalse}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({topics: state.topicReducer})
+}
+
+export default connect(mapStateToProps)(TopicResult);
--- src/routes/TopicResultPage/index.js
+++ src/routes/TopicResultPage/index.js
@@ -1,83 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
-
-
-
+import TopicResult from '../../containers/TopicResult';
 
 class TopicsResultPage extends Component {
-  // postVote(id, selection){
-  //
-  //   // dispatch an action to update redux state
-  //   // button yes no variable
-  //   var myHeaders = new Headers({'Content-Type': 'application/json'});
-  //   var myBody = { "selection": false };
-  //   var myInit = {
-  //            method: 'POST',
-  //            headers: myHeaders,
-  //            body: JSON.stringify(myBody)
-  //          }
-  //
-  //     fetch('http://localhost:8080/topics/' + this.props.match.params.id + '/vote', myInit)
-  //     .then(parseJSON => parseJSON.json())
-  //     .then(data => {
-  //       console.log("data ", data);
-  //     });
-  //
-  //     // now dispatch an action
-  // }
-
-
-  componentDidMount(){
-      fetch('http://localhost:8080/topics')
-      .then(parseJSON => parseJSON.json())
-      .then(data => {
-        this.props.dispatch({
-        type: 'GETTOPICS',
-        topics: data,
-        })
-      });
-  }
-
-
 
   render(){
-    const topic = this.props.topics[this.props.match.params.id-1]
-    console.log("resultPage", topic);
-    const total = topic.votes.length;
-
-    let voteTrue = 0;
-    const countTrue = topic.votes.filter((vote) => {
-      if(vote.selection === true){
-        voteTrue++;
-      }
-      return voteTrue;
-    });
-
-    let voteFalse = 0;
-    const countFalse = topic.votes.filter((vote) => {
-      if(vote.selection === false){
-        voteFalse++;
-      }
-      return voteFalse;
-    });
 
     return(
       <div>
-        <ul>
-          total: {total} <br></br>
-          yes: {voteTrue}<br></br>
-          no: {voteFalse}
-        </ul>
+      <TopicResult paramsId = {this.props.match.params.id}/>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return({
-    topics: state.topicReducer
-  })
-}
-
-export default connect(mapStateToProps)(TopicsResultPage);
+export default connect()(TopicsResultPage);
